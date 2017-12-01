a7a3ddbcbcae94e1e3190eed077f06ab12af885e
sunmaer <878131732@qq.com>
2017-12-01 11:15:02 +0800

    增加图标

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
-import router from './router'
+import router from './router/router'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
