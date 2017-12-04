ab42e03e1c9454145ad0d1d4aa042add94926ebf
sunmaer <878131732@qq.com>
2017-12-04 10:50:16 +0800

    引入第三方图标库

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/views/login/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/test',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
     }
   ]
 })
---
+++ src/views/login/login.vue
@@ -0,0 +1,60 @@
+<template lang="html">
+  <el-row class="main">
+    <el-card class="login__box">
+      <div slot="header" class="login__header">
+        <span>卡片名称</span>
+      </div>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card>
+  </el-row>
+</template>
+
+<script type="text/javascript">
+  export default {
+    // 不要忘记了 name 属性
+    name: 'RangeSlider',
+    // 组合其它组件
+    extends: {},
+    // 组件属性、变量
+    props: {
+      bar: {}, // 按字母顺序
+      foo: {},
+      fooBar: {}
+    },
+    // 变量
+    data () {},
+    computed: {},
+    // 使用其它组件
+    components: {},
+    // 方法
+    watch: {},
+    methods: {},
+    // 生命周期函数
+    beforeCreate () {},
+    mounted () {}
+  }
+</script>
+
+<style scoped lang="scss">
+  .main {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-image: url('../../assets/login_bg.jpg');
+    background-size: cover;
+    // background-position: 50%;
+  }
+  .login__box {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-60%);
+    right: 160px;
+    width: 350px;
+  }
+  .login__header {
+    font-weight: bold;
+    text-align: left;
+  }
+</style>
