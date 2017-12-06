ee3dee9a6bfeafd2666ddd0b19fbfa82a49ac166
sunmaer <878131732@qq.com>
2017-12-06 16:51:51 +0800

    增加面包屑导航

--- src/views/admin/admin.vue
+++ src/views/admin/admin.vue
@@ -4,6 +4,12 @@
     <el-container class="main">
       <side-bar></side-bar>
       <el-main>
+        <el-breadcrumb separator-class="el-icon-arrow-right" class="main__bread">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+          <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+          <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
         <router-view></router-view>
       </el-main>
     </el-container>
@@ -29,5 +35,8 @@
     top: 60px;
     bottom: 0;
     width: 100%;
+    .main__bread {
+      margin-bottom: 20px;
+    }
   }
 </style>
