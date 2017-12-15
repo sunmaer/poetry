a10798ab7eeb82b67252cdcfc0c1f214bf80c81b
sunmaer <878131732@qq.com>
2017-12-15 10:58:04 +0800

    修改统计组件

--- src/views/index/countInfo.vue
+++ src/views/index/countInfo.vue
@@ -2,11 +2,12 @@
   <el-card class="count"  :body-style="{ padding: '0px' }">
     <el-row>
       <el-col :span="8" class="count__icon" :style="{ backgroundColor: bgColor }">
-        <i :class="['fa', 'fa-'+icon, 'fa-3x']"></i>
+        <i :class="['fa', 'fa-'+icon, 'fa-2x']"></i>
       </el-col>
       <el-col :span="16">
         <el-row class="count__number"  :style="{ color: bgColor }">{{ number }}</el-row>
-        <el-row class="count__types">{{ type }}</el-row>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+        <el-row class="count__types">{{ type }}<a v-if="update" class="count__btn" href="javascript:void(0);">修改</a></el-row>
       </el-col>
     </el-row>
   </el-card>
@@ -31,10 +32,16 @@
       type: {
         type: String,
         default: '总数'
+      },
+      update: {
+        type: Boolean,
+        default: false
       }
     },
     data () {
-      return {}
+      return {
+
+      }
     }
   }
 </script>
@@ -42,7 +49,7 @@
 <style lang="scss" scoped>
   .count {
     width: 100%;
-    height: 210px;
+    height: 140px;
     color: #657180;
     text-align: center;
     box-shadow: none;
@@ -50,17 +57,25 @@
       box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
     }
     .count__icon {
-      line-height: 210px;
+      line-height: 140px;
       color: #ffffff;
+      .fa {
+        margin-top: 60px;
+      }
     }
     .count__number {
-      margin-top: 65px;
+      margin-top: 35px;
       margin-bottom: 10px;
       font-size: 30px;
       font-weight: bold;
     }
     .count__types {
       color: #c8c8c8;
+      .count__btn {
+        margin-left: 5px;
+        color: #409EFF;
+        text-decoration: none;
+      }
     }
   }
 </style>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,12 +1,15 @@
 <template>
   <el-row :gutter="20">
-    <el-col :span="10">
+    <el-col :span="9">
       <admin-info></admin-info>
     </el-col>
-    <el-col :span="7">
+    <el-col :span="5">
+      <count-info bgColor="#409EFF" icon="tasks" number="50" type="总题量" update="true" ></count-info>
+    </el-col>
+    <el-col :span="5">
       <count-info bgColor="#67C23A" icon="user-plus" number="412" type="学生总数"></count-info>
     </el-col>
-    <el-col :span="7">
+    <el-col :span="5">
       <count-info bgColor="#EB9E05" icon="edit" number="250" type="题目总数" ></count-info>
     </el-col>
   </el-row>
