77b51b81e4f2a66334d88365620cc14559638c13
sunmaer <878131732@qq.com>
2018-05-20 22:03:28 +0800

    获取题库

--- src/views/home/test.vue
+++ src/views/home/test.vue
@@ -14,13 +14,13 @@
       <el-row class="test__question">
         <section>
           <h3>一、选择题（共5题，每题5分。每题只有一个答案是正确的，请从四个备选答案中选出正确选项）</h3>
-          <el-row v-for="(choice,index) in choices" :key="index" class="question__item">
-            <h4  :id="'choiceOrder'+index">{{index+1}}.{{choice.question}}</h4>
-            <el-radio-group v-model="choiceradio[index]" class="item" >
-              <el-radio label="1">{{choice.option[0]}}</el-radio>
-              <el-radio label="2">{{choice.option[1]}}</el-radio>
-              <el-radio label="3">{{choice.option[2]}}</el-radio>
-              <el-radio label="4">{{choice.option[3]}}</el-radio>
+          <el-row v-for="(choice,index) in choice" :key="index" class="question__item">
+            <h4 :id="'choiceOrder'+index">{{index+1}}.{{choice.question}}</h4>
+            <el-radio-group v-model="choiceAnswer[index]" class="item" >
+              <el-radio label="1">{{choice.option && choice.option[0]}}</el-radio>
+              <el-radio label="2">{{choice.option && choice.option[1]}}</el-radio>
+              <el-radio label="3">{{choice.option && choice.option[2]}}</el-radio>
+              <el-radio label="4">{{choice.option && choice.option[3]}}</el-radio>
             </el-radio-group>
           </el-row>
         </section>
@@ -29,7 +29,7 @@
           <h3>二、判断题（共5题，每题5分。请根据题目内容选择正确或者错误）</h3>
           <el-row v-for="(judge,index) in judges" :key="index" class="question__item">
             <h4 :id="'judgeOrder'+index">{{index+1}}. {{judge.question}}</h4>
-            <el-radio-group v-model="judgeradio[index]" class="item">
+            <el-radio-group v-model="judgeAnswer[index]" class="item">
               <el-radio label="1">正确</el-radio>
               <el-radio label="0">错误</el-radio>
             </el-radio-group>
@@ -40,11 +40,11 @@
           <h3>三、欣赏题（共5题，每题5分。请欣赏完作品后完成选项，每题只有一个答案是正确的，请从四个备选答案中选出正确选项）</h3>
           <el-row v-for="(admiring,index) in admirings" :key="index" class="question__item">
             <h4 :id="'admiringOrder'+index">{{index+1}}.{{admiring.question}}</h4>
-            <el-radio-group v-model="admiringradio[index]" class="item">
-              <el-radio :label="1">{{admiring.option[0]}}</el-radio>
-              <el-radio :label="2">{{admiring.option[1]}}</el-radio>
-              <el-radio :label="3">{{admiring.option[2]}}</el-radio>
-              <el-radio :label="4">{{admiring.option[3]}}</el-radio>
+            <el-radio-group v-model="admiringAnswer[index]" class="item">
+              <el-radio :label="1">{{admiring.option && admiring.option[0]}}</el-radio>
+              <el-radio :label="2">{{admiring.option && admiring.option[1]}}</el-radio>
+              <el-radio :label="3">{{admiring.option && admiring.option[2]}}</el-radio>
+              <el-radio :label="4">{{admiring.option && admiring.option[3]}}</el-radio>
             </el-radio-group>
           </el-row>
         </section>
@@ -117,13 +117,16 @@ export default {
         class:'',
         id:''
       },
-      LoginVisible: false,
-      choiceradio:[],
+      LoginVisible: true,
+      choice:[],
       choiceNum: 0,
-      judgeradio:[],
+      choiceAnswer: [],
+      judge:[],
       judgeNum: 0,
-      admiringradio:[],
+      judgeAnswer: [],
+      admiring:[],
       admiringNum: 0,
+      admiringAnswer: []
     }
   },
   created:function(){
@@ -131,6 +134,21 @@ export default {
     this.choiceNum = this.$route.query.choiceNum || 0
     this.judgeNum = this.$route.query.judgeNum || 0
     this.admiringNum = this.$route.query.admiringNum || 0
+
+    // 获取题库
+    this.$axios({
+      method: 'POST',
+      url: `${API_HOST}Poetry/Library`,
+      data: [this.choiceNum, this.judgeNum, this.admiringNum]
+    }).then((res) => {
+      if(res.status) {
+        this.choice = res.data.choice
+        this.judge = res.data.judge
+        this.admiring = res.data.admiring
+      }
+    }).catch((err) => {
+      this.$message.error(`题库获取失败 ${err}`)
+    })
   },
   methods:{
     login(){
@@ -173,7 +191,7 @@ export default {
         data: {
           name: this.registerForm.name,
           class: this.registerForm.class,
-          id: this.id
+          id: this.registerForm.id
         }
       }).then((res) => {
         if(!res.data.status) {
@@ -186,71 +204,12 @@ export default {
       })
     },
     submit:function(){
-      var check = /^\d$/;
-      for(var i=0;i<this.choiceradio.length;i++){
-        if(!check.test(this.choiceradio[i])){
-          alert('选择题未填写完整');
-          return;
-        }
+      // 登录才可以提交试卷
+      if(!this.isLogin) {
+        this.$message.warning('登录以后才可以提交试卷')
+        return false
       }
-      for(var i=0;i<this.judgeradio.length;i++){
-        if(!check.test(this.judgeradio[i])){
-          alert('判断题未填写完整');
-          return;
-        }
-      }
-      for(var i=0;i<this.admiringradio.length;i++){
-        if(!check.test(this.admiringradio[i])){
-          alert('欣赏题未填写完整');
-          return;
-        }
-      }
-      var answers = this.choiceradio.concat(this.judgeradio.concat(this.admiringradio));
-      var selectarr = new Array('0','A','B','C','D');
-      var judgearr = new Array('F','T');
-      axios.post(API_HOST+'Poetry/Grade',{
-            id:this.id,
-            answer:answers
-          })
-          .then(function(res){
-            res = res.data;
-            if(res.status = true){
-              var i,j,k;
-              var wronganswer,answerid;
-              //显示错误题目，并标注正确答案
-              for(i=0;i<this.choiceradio.length;i++){
-                if(this.choiceradio[i] != res.answer[i]){
-                  answerid = 'choiceOrder' + i;
-                  wronganswer = document.getElementById(answerid);
-                  wronganswer.style.color = 'red';
-                  wronganswer.innerHTML += '正确答案：'+ selectarr[res.answer[i]];
-                }
-              }
-              for(j=0;j<this.judgeradio.length;j++){
-                if(this.judgeradio[j] != res.answer[i+j]){
-                  answerid = 'judgeOrder' + j;
-                  wronganswer = document.getElementById(answerid);
-                  wronganswer.style.color = 'red';
-                  wronganswer.innerHTML += '正确答案：'+ judgearr[res.answer[i+j]];
-                }
-              }
-              for(k=0;k<this.admiringradio.length;k++){
-                if(this.admiringradio[k] != res.answer[i+j+k]){
-                  answerid = 'admiringOrder' + k;
-                  wronganswer = document.getElementById(answerid);
-                  wronganswer.style.color = 'red';
-                  wronganswer.innerHTML += '正确答案：'+ selectarr[res.answer[i+j+k]];
-                }
-              }
-              var info = "本次测试得分为：" + res.data.grade;
-              alert(info);
-              
-            }
-          })
-          .catch(function(err){
-            console.log(err);
-            alert("提交失败，请检查信息");
-          });
+
     },
   }
 }
