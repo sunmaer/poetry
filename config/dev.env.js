ac7f9cc96d79723bed51ec8bdd164ab815c8e330
zjbtmdzjb <zjb770168382@qq.com>
2018-07-28 15:52:19 +0800

    环境

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',    //开发环境
-  API_HOST:'"//www.vidahaha.top:7002/"'
-//  API_HOST:'"/localhost:7002/"'
+//  API_HOST:'"//www.vidahaha.top:7002/"'
+  API_HOST:'"/localhost:7002/"'
 })
